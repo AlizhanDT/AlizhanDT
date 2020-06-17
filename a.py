f59d56d917d11d4b6d4d01cdb87e4204f782cdda
AlizhanDT <66683482+AlizhanDT@users.noreply.github.com>
2020-06-17 23:40:37 +0600

    Add files via upload

--- a.py
+++ a.py
@@ -1,4 +1,10 @@
-a=int(input())
-b=int(input())
-
-print (a+b)                
+n=input()
+l=len(n)
+b=True
+for i in range(l//2):
+    if n[i]!=n[-1-i]:
+        b=False
+if b:
+    print("YES")
+else:
+    print("NO")
